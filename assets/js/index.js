9961598c48c5017937904c262b46d91408e5d718
GabrieleBarbuto <gabba.b2001@gmail.com>
2024-05-10 12:01:12 +0200

    salvataggio presentazione

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+
+
+const slides = document.querySelectorAll('.slide');
+        let currentIndex = 0;
+      
+        function showSlide(index) {
+          slides.forEach((slide, i) => {
+            slide.style.transform = `translateX(${100 * (i - index)}%)`;
+          });
+        }
+      
+        function nextSlide() {
+          currentIndex = (currentIndex + 1) % slides.length;
+          showSlide(currentIndex);
+        }
+      
+        function prevSlide() {
+          currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+          showSlide(currentIndex);
+        }
+
+        function showSlide(index) {
+          slides.forEach((slide, i) => {
+          slide.style.transform = `translateX(-${100 * index}%)`;
+  });
+}
+
+
+
+
+
